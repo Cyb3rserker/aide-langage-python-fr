e2dc7d0dee7354a53559183c6aecddbf80f878e2
MedouneDiop <med.diop7829@gmail.com>
2021-05-12 21:52:54 +0000

    Explication des cast supprimée de variables.py et mis dans un nouveau fichier aide/donnee/cast.py.

--- donnee/variables.py
+++ donnee/variables.py
@@ -17,7 +17,7 @@ Les différents types de données standard :
     - entier numérique (int)
     - nombre flottant (float)
     - chaîne de caractères (str)
-    - booléen (bool) qui ne peut avoir que deux valeurs (vrai / faux, 0 / 1...)
+    - booléen (bool) qui ne peut avoir que deux valeurs : True ou False
 """
 
 """
@@ -132,34 +132,7 @@ voir aide/fonction/lecture_donnees
 
 """
 On peut parfois avoir le besoin de changer le type de donnée d'une variable pour effectuer une
-opérations par exemple, et dans ce cas on "cast" la variable, c'est à dire qu'on la convertie.
-Voici un exemple :
-
-double_age_majorite = age_majorite + age_majorite2
-
-Ce calcul est impossible car on tente d'additionner un entier avec une chaîne de caractères.
-Si on veut additionner les deux variables, il faut les caster :
-"""
-
-double_age_majorite = age_majorite + int(age_majorite2)
-# (On additionne un entier avec un entier donc double_age_majorite sera un entier)
-print("Le double de l'âge de la majorité est", double_age_majorite, "ans.\n")
-"""
-sortie au terminal :
-
-Le double de l'âge de la majorité est 36 ans.
-"""
-
-"""
-Il existe autant de cast que de type de donnée comme :
-- int(),
-- float(),
-- str(),
-- bool()
-- et plein d'autres encore.
-"""
-
-"""
-ATTENTION !!! Le cast ne change pas le type de donnée de la variable définitivement, elle est
-juste interprétée différement lorsqu'on la cast.
+opérations par exemple, et dans ce cas on "cast" la variable, c'est à dire qu'on change son type
+temporairement pour que l'interpréteur traite la variable différement. Pour avoir plus de
+renseignements, voir aide/donnee/cast.py .
 """

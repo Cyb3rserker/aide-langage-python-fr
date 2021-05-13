aeb0cd2d506285dc69428d22198176964b9f099c
MedouneDiop <med.diop7829@gmail.com>
2021-05-13 16:40:46 +0000

    operateurs.py fini

--- divers/operateurs.py
+++ divers/operateurs.py
@@ -12,6 +12,23 @@ classiques. Les différents opérateurs en python, et dans la programmation en g
         ex : 10 % 8 = 2
         il peut être très utile et peut notament servir à savoir si un nombre est pair ou
         pas par exemple :
-        si ma_variables % 2 n'est pas égal à 0, alors...
+        si ma_variable % 2 n'est pas égal à 0, alors...
         (le reste d'une division Euclidienne d'un nombre pair par 2 est forcément nul)
+
+On peut utiliser les opérateurs pour changer la valeurs d'une variables ) :
 """
+ma_variable = 5
+ma_variable = ma_variable + 1
+# ma_variable sera alors égal à 6.
+
+# On peut aussi utiliser une autre syntaxe :
+
+ma_variable += 1
+# Ici, ma_variable est égal à 7
+
+"""
+C'est une syntaxe équivalent qui fait exactement la même chose que les lignes
+au dessus et on peut le faire avec tous les opérateurs :
+"""
+ma_variable *= 2
+# Ici, ma_variable est égal à 14

6cf23d5b8c5e1b890ef341c17519718f42d32601
MedouneDiop <med.diop7829@gmail.com>
2021-06-08 10:34:26 +0200

    propriete_encapsulation.py bientôt terminé il ne reste plus que l'attribut delete

--- prog_oriente_objet/propriete_encapsulation.py
+++ prog_oriente_objet/propriete_encapsulation.py
@@ -72,9 +72,109 @@ class Chien :
         self._taille = c_taille
         print("Chien généré !\n\nRace : {}\nTaille :{}\n".format(c_race, c_taille))
 
+    """
+    Dans mon exemple, il faut une sorte de mot de passe 
+    administrateur pour lire ou changer les attributs d'un Chien.
+    Je vais donc créer des propriétés pour age et race 
+    grâce à property, une méthode ayant les arguments 
+    suivant :
+
+    property(<getter>, <setter>, <deleter>, <helper>)
+
+    - getter  : obtenir la valeur d'un attribut
+    - setter  : modifier la valeur d'un attribut
+    - deleter :
+    - helper  :
+
+    Ces arguments ne sont pas obligatoire et on peut 
+    juste se contenter du getter dans notre cas. On va 
+    d'abord définir une méthode qui retourne l'attribut 
+    _age seulement si on note le bon mot de passe 
+    administrateur :
+    """
+
+    mdp_admin = "1234"
+
+    """
+    Ici, on va autorisé la lecture de l'age seulement 
+    si l'utilisateur tape le bon mot de passe :
+    """
+    def _getrace(self) :
+        mdp_user = input("Mot de passe pour connaitre la race : ")
+
+        if mdp_user == Chien.mdp_admin :
+            print("Accès autorisé.")
+            return self._race
+        else :
+            print("Accès non autorisé.")
+
+    """
+    Là, on va faire une propriété qui va limiter la 
+    possibilité de modifier un attribut, ici la race :
+    """
+    def _setrace(self, nouvelle_race) :
+        mdp_user = input("Mot de passe pour changer la race : ")
+        if mdp_user == Chien.mdp_admin :
+            print("Accès autorisé.")
+            self._race = nouvelle_race
+        else :
+            print("Accès non autorisé.")
+        
+    # On créé ensuite la propriété : 
+    race = property(_getrace, _setrace)
+    # note : ici, race est le nom de la propriété
+    """
+    """
+
+# Début du programme :
 
 chien_1 = Chien("Bulldog", "MOYEN")
 
-print("Race du chien n°1 :", chien_1.race)
+print("\n_____UTILISATION DU GETTER_____")
+print("(mdp : 1234)\n")
+
+print("Race du chien n°1 : ", chien_1.race)
+                             # Ici, c'est la propriété 
+                             # race et pas l'attribut 
+                             # _race.
+
+"""
+sortie au terminal :
+
+Génération d'un chien...
+Chien généré !
+
+Race : Bulldog
+Taille :MOYEN
+
+
+_____UTILISATION DU GETTER_____
+(mdp : 1234)
+
+Mot de passe pour connaitre la race : 1234
+Accès autorisé.
+Race du chien n°1 :  Bulldog
+"""
+
+print("\n_____UTILISATION DU SETTER EN PLUS_____\n")
+print("(mdp : 1234)")
+
+print("Race chien n°1 :", chien_1.race)
 chien_1.race = "Rottweiler"
-print("Race du chien n°1 :", chien_1.race)
+print("Race chien n°1 :", chien_1.race)
+
+"""
+sortie au terminal :
+
+_____UTILISATION DU SETTER EN PLUS_____
+
+(mdp : 1234)
+Mot de passe pour connaitre la race : 1234
+Accès autorisé.
+Race chien n°1 : Bulldog
+Mot de passe pour changer la race : 1234 
+Accès autorisé.
+Mot de passe pour connaitre la race : 1234 
+Accès autorisé.
+Race chien n°1 : Rottweiler
+"""

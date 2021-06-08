34db04ab3c79ce8a6aa09e8e2a0b52bbec5163a1
MedouneDiop <med.diop7829@gmail.com>
2021-06-08 23:29:21 +0200

    propriété d'encapsulation fini. Il y aura peut-être des mises à jours dans le futur.

--- prog_oriente_objet/propriete_encapsulation.py
+++ prog_oriente_objet/propriete_encapsulation.py
@@ -54,7 +54,7 @@ les attributs ce qui empêche d'y avoir accès.
 
 class Chien :
     """
-    documentation :
+    Documentation de Chien :
     - c_race : ce que l'on veut
     - c_taille :
         - "PETIT"
@@ -76,15 +76,16 @@ class Chien :
     Dans mon exemple, il faut une sorte de mot de passe 
     administrateur pour lire ou changer les attributs d'un Chien.
     Je vais donc créer des propriétés pour age et race 
-    grâce à property, une méthode ayant les arguments 
-    suivant :
+    grâce à property, une méthode ayant les arguments (ou 
+    "accesseurs" suivant :
 
     property(<getter>, <setter>, <deleter>, <helper>)
 
     - getter  : obtenir la valeur d'un attribut
     - setter  : modifier la valeur d'un attribut
-    - deleter :
-    - helper  :
+    - deleter : supprime l'attribut d'un objet
+    - helper  : donne la documentation d'une classe ou 
+                d'un attribut
 
     Ces arguments ne sont pas obligatoire et on peut 
     juste se contenter du getter dans notre cas. On va 
@@ -123,8 +124,54 @@ class Chien :
     # On créé ensuite la propriété : 
     race = property(_getrace, _setrace)
     # note : ici, race est le nom de la propriété
+
     """
+    Le troisième argument de property est le deleter. Il permet 
+    de supprimer l'attribut d'un objet. Voici un exemple :
     """
+
+    def _deltaille(self) :
+        del self._taille
+    """
+    Le mot clé def va supprimer l'attribut taille de l'objet
+    et si l'on tente d'y accéder, on rencontrera une AttributeError. 
+    On peut donc ce servir de ce code d'erreur pour améliorer le getter 
+    de l'attribut 
+    """
+    def _gettaille(self) :
+        try :
+            return self._taille
+        except AttributeError :
+            print("La taille pour ce chien n'existe pas !")
+            return ""
+            """
+            Je met cette ligne sinon le print affichera "None" dans 
+            le terminal car l'attribut auquel on essaie d'accéder 
+            n'existe pas donc pour plus de propreté, j'ai retourné 
+            une simple ligne.
+            """
+
+    def _settaille(self, nouvelle_taille) :
+        try : 
+            self._taille = nouvelle_taille
+        except AttributeError :
+            print("La taille pour ce chien n'existe pas !")
+    
+    """
+    Enfin, le dernier argument est l'helper. Cette argument est 
+    souvent une chaîne de caractère qui contient une description 
+    de notre attribut :
+    """
+
+    documentation_taille = '''__Documentation de _taille :__
+                            L'attribut taille contient la taille
+                            d'un chien. Ses différentes valeurs
+                            possibles sont :
+                            - PETIT
+                            - MOYEN
+                            - GRAND'''
+
+    taille = property(_gettaille, _settaille, _deltaille, documentation_taille)
 
 # Début du programme :
 
@@ -177,4 +224,98 @@ Accès autorisé.
 Mot de passe pour connaitre la race : 1234 
 Accès autorisé.
 Race chien n°1 : Rottweiler
+"""
+
+print("\n_____UTILISATION D'UN DELETER_____\n")
+
+print("Taille du chien n°1 avant suppression attribut : ") 
+print(chien_1.taille)
+
+del chien_1.taille
+
+print("Taille du chien n°1 après suppression attribut : ") 
+print(chien_1.taille)
+
+"""
+sortie au terminal :
+
+_____UTILISATION D'UN DELETER_____
+
+Taille du chien n°1 avant suppression attribut :
+MOYEN
+Taille du chien n°1 après suppression attribut :
+La taille pour ce chien n'existe pas !
+
+"""
+
+print("\n_____UTILISATION DE HELP_____\n")
+
+"""
+La fonction help permet d'obtenir des informations sur un élément, 
+et dans notre cas ce sera une classe et un attribut :
+"""
+
+help(Chien)
+"""
+sortie au terminal :
+
+_____UTILISATION DE HELP_____
+
+Help on class Chien in module __main__:
+
+class Chien(builtins.object)
+ |  Chien(c_race, c_taille)
+ |
+ |  Documentation de Chien :
+ |  - c_race : ce que l'on veut
+ |  - c_taille :
+ |      - "PETIT"
+ |      - "MOYEN"
+ |      - "GRAND"
+ |
+ |  Methods defined here:
+ |
+ |  __init__(self, c_race, c_taille)
+ |      Initialize self.  See help(type(self)) for accurate signature.
+ |
+ |  ----------------------------------------------------------------------
+ |  Data descriptors defined here:
+ |
+ |  __dict__
+ |      dictionary for instance variables (if defined)
+ |
+ |  __weakref__
+ |      list of weak references to the object (if defined)
+ |
+ |
+ |  taille
+ |      __Documentation de _taille :__
+ |                                  L'attribut taille contient la taille
+        d'un chien. Ses diffÚrentes valeurs                             possibles sont :     
+ |                                  - PETIT
+ |                                  - MOYEN
+ |                                  - GRAND
+ |
+ |  ----------------------------------------------------------------------
+ |  Data and other attributes defined here:
+ |
+ |  documentation_taille = '\n__Documentation de _taille :__\n            ...
+ |
+ |  mdp_admin = '1234'
+
+"""
+
+help(Chien.taille)
+
+"""
+sortie au terminal :
+
+Help on property:
+    __Documentation de _taille :__
+                                L'attribut taille contient la taille
+                                d'un chien. Ses différentes valeurs
+                                possibles sont :
+                                - PETIT
+                                - MOYEN
+                                - GRAND'
 """

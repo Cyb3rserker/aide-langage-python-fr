45a81b6eab1739866bc0c130bddc72a568876830
MedouneDiop <med.diop7829@gmail.com>
2021-06-09 21:01:06 +0200

    structure_fichier_python.py fini. Peut-être des mises à jour à l'avenir.

--- structure_fichier_python.py
+++ structure_fichier_python.py
@@ -2,6 +2,21 @@
 
 """
 La ligne du dessus permet de définir le cryptage des caractères. 
-utf-8 est le plus recommendé car il prend tout les caractères de 
-toutes les langues. 
+utf-8 est le plus recommendé car il prend tout les caractères 
+spéciaux de toutes les langues. 
+"""
+
+"""
+En python, on utilise des fonctions. Ce sont des algoritmes qui 
+permettent d'effectuer certaines actions. Pour plus de renseignement, 
+voir le fichier aide/fonction/fonction.py. Par exemple, je vais là 
+utiliser la fonction print qui permet d'afficher des éléments sur 
+le terminal. Pour plus d'information, voir aide/fonction/affichage/print.py.
+"""
+
+print("Bonjour je suis une phrase étant dans un fichier expliquant le fonctionnement d'un fichier python.")
+
+"""
+C'est ici que ce clôture ce fichier d'aide très simple. Pour approfondire 
+vos connaissance, regardez les autres fichiers du dossier aide.
 """
